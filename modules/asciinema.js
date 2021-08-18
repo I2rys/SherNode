5fc4a4239b9442b96116869ff27a8c6a27b56991
I2rys <i2rys@protonmail.com>
2021-08-17 19:51:53 -0700

    Create asciinema.js

---
+++ modules/asciinema.js
@@ -0,0 +1,37 @@
+//Main
+function self(name, callback){
+    var final_name = ""
+
+    for( i in name ){
+        if(final_name.length == 0){
+            final_name = name[i]
+        }else{
+            final_name += `${name[i]}`
+        }
+    }
+
+    if(name.length == 1){
+        var to_callback = {
+            website: "https://asciinema.org/~",
+            parsed_name: final_name,
+            sfae: `body > main > div > section > div > span > img`
+        }
+
+        callback(to_callback)
+        return
+    }else{
+        var to_callback = {
+            website: "https://asciinema.org/~",
+            parsed_name: final_name,
+            sfae: `body > main > div > section > div > span > img`
+        }
+
+        callback(to_callback)
+        return
+    }
+}
+
+//Exporter
+module.exports = {
+    self: self
+}
